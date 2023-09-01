6c8f4caa1093a63dd5d197498afd1e0be42394e4
Arun R <arunraja711200@gmail.com>
2023-09-01 13:16:29 +0530

    Add files via upload

---
+++ Arun Blog.Web/Models/Domain/ViewModel/EditTagRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace ArunBlog.Web.Models.Domain.ViewModel
+{
+    public class EditTagRequest
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string DisplayName { get; set; }
+    }
+}
---
+++ Arun Blog.Web/Models/Domain/ViewModel/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ArunBlog.Web.Models.Domain.ViewModel
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<BlogPost> blogPosts { get; set; }
+
+        public IEnumerable<Tag> Tags { get; set; }
+    }
+}
