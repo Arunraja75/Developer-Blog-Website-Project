862a8264b6bda885baeb5034c95205dd4602c819
Arun R <arunraja711200@gmail.com>
2023-09-01 12:56:46 +0530

    Add files via upload

---
+++ Arun Blog.Web/Data/BlogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ArunBlog.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace ArunBlog.Web.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+        }
+        public DbSet<BlogPost> BlogPosts{ get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
